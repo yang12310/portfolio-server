51fbe33e7dfad3bd825d1155007796fdcddf241b
yang12310 <yang12310@naver.com>
2024-09-24 11:36:57 +0900

    240924

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,7 +4,6 @@ export const verifyToken = async (req, res, next) => {
   try {
     let token = req.header("Authorization");
 
-
     if (!token) {
       return res.status(403).send("Access Denied");
     }
